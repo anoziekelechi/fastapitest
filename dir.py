11eceedcd5a05cff072d910c5b9c3d19c5661720
anoziekelechi <68008223+anoziekelechi@users.noreply.github.com>
2026-05-17 20:37:54 +0000

    Create dir.py

---
+++ dir.py
@@ -0,0 +1,5 @@
+anoziekelechi@Anozies-MacBook-Pro Ecommerce % ls -a
+.                               .env.production                 docker-compose-production.yml   Makefile
+..                              .git                            docker-compose.override.yml     nginx
+.dockerignore                   .gitignore                      docker-compose.yml
+.env.development                backend
